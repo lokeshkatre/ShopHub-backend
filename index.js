36d5b0db3c2b4ae12b764c76db5f61b816117e54
Lokesh <lokeshkkatre123@gmail.com>
2024-05-23 11:58:10 +0530

    Upload changes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const path = require("path");
 const cors = require("cors");
 const fs = require("fs");
 const bcrypt = require('bcryptjs');
+const { BASE_URL } = require("../admin/src/utils/utils");
 
 const app = express();
 const port = process.env.PORT||4000; // Use environment variable or fallback to 4000
@@ -60,7 +61,7 @@ app.post("/upload", upload.single('product'), (req, res) => {
     }
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `${BASE_URL}/images/${req.file.filename}`
     });
 });
 
