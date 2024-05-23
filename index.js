13463a26c511aa54821ddae974a6001d2da06854
Lokesh <lokeshkkatre123@gmail.com>
2024-05-23 12:04:33 +0530

    upload changes

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const path = require("path");
 const cors = require("cors");
 const fs = require("fs");
 const bcrypt = require('bcryptjs');
-const { BASE_URL } = require("../admin/src/utils/utils");
 
 const app = express();
 const port = process.env.PORT||4000; // Use environment variable or fallback to 4000
@@ -61,7 +60,7 @@ app.post("/upload", upload.single('product'), (req, res) => {
     }
     res.json({
         success: 1,
-        image_url: `${BASE_URL}/images/${req.file.filename}`
+        image_url: `https://shophub-8g87.onrender.com/images/${req.file.filename}`
     });
 });
 
